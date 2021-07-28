9120eabb3b35c8d53190f523d71a7b390ed42f56
Kevin Bowersox <kmb385@gmail.com>
2021-07-28 04:25:15 -0400

    updated

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,5 +1,6 @@
 package com.linkedin.collections;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -11,12 +12,32 @@ public class Application {
 		Guest sonia = new Guest("Sonia", "Doe", true);
 		Guest siri = new Guest("Siri", "Doe", true);
 
+		List<Guest> checkinList = new ArrayList<>(100);
+
+		checkinList.add(john);
+		print(checkinList);
+		
+		checkinList.add(maria);
+		print(checkinList);
+		
+		checkinList.add(0, sonia);
+		print(checkinList);
+		
+		checkinList.get(2).setLoyaltyProgramMember(true);
+		checkinList.addAll(1, List.of(maria, siri));
+		
+		print(checkinList);
+		
+		checkinList.remove(checkinList.size() - 1);
+		print(checkinList);
+		
+		System.out.format("%nJohn is here %d", checkinList.indexOf(john));
 
 	}
 
 	public static void print(List<Guest> list) {
 
-		System.out.format("%n--List Contents--%n");
+		System.out.println(System.lineSeparator() + "--List Contents--");
 
 		for (int x = 0; x < list.size(); x++) {
 			Guest guest = list.get(x);
